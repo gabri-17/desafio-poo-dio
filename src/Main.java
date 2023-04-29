3d21f29fb4d211e77e467949194fb2fabd31fef8
Gabriel <ga170120@gmail.com>
2023-04-28 22:22:25 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Aprenda java - POO");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso c#");
+        curso2.setDescricao("Aprenda c# - POO");
+        curso2.setCargaHoraria(10);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
