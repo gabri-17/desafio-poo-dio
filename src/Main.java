9f93f4cf8859d0f6892e13f27311c3699a62d66b
Gabriel <ga170120@gmail.com>
2023-04-28 23:58:13 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import br.com.desafio.dominio.Curso;
-import br.com.desafio.dominio.Mentoria;
+import br.com.desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,15 +16,57 @@ public class Main {
         curso2.setDescricao("Aprenda c# - POO");
         curso2.setCargaHoraria(10);
 
+        Conteudo conteudo = new Curso();
+
+
+        //Polimorfismo: instanciou um Array list a partir da classe List.
+        /*Tudo o que tem dentro de List tem dentro
+        de ArrayList, mas nem tudo que tem dentro
+        ArrayList tem dentro de List.*/
+        //List<String> palavras = new ArrayList<>();
+
+
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+       /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudo().add(curso1);
+        bootcamp.getConteudo().add(curso2);
+        bootcamp.getConteudo().add(mentoria);
+
+        Dev devGabriel = new Dev();
+        devGabriel.setNome("Gabriel");
+        devGabriel.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Gabriel" + devGabriel.getConteudosInscritos());
+
+        devGabriel.progredir();
+        devGabriel.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Gabriel" + devGabriel.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Oswaldo" + devGabriel.getConteudoConcluidos());
+        System.out.println("XP: " + devGabriel.calcularTotalXp());
+
+        System.out.println("------------");
+        Dev devOswaldo = new Dev();
+        devOswaldo.setNome("Oswaldo");
+        devOswaldo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Oswaldo" + devOswaldo.getConteudosInscritos());
+        devOswaldo.progredir();
+        devOswaldo.progredir();
+        devOswaldo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Oswaldo" + devOswaldo.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Oswaldo" + devOswaldo.getConteudoConcluidos());
+        System.out.println("XP: " + devOswaldo.calcularTotalXp());
 
     }
 }
---
+++ src/br/com/desafio/dominio/Dev.java
@@ -0,0 +1,82 @@
+package br.com.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudo());
+        bootcamp.getDevInscritos().add(this);
+    }
+
+    public void progredir(){
+        /*
+        * Optional é um objeto contêiner que pode
+        * ou não conter um valor não nulo. Em vez
+        * de retornar null, você pode retornar um
+        * objeto Optional que contém um valor ou
+        * está vazio.
+        *
+        * O uso de Optional pode ajudar a evitar
+        * NullPointerExceptions e tornar o código
+        * mais robusto e fácil de manter.
+        * */
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
+
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos
+                .stream().
+                mapToDouble(Conteudo::calcularXP)
+                .sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudoConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudoConcluidos(Set<Conteudo> conteudoConcluidos) {
+        this.conteudosConcluidos = conteudoConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+}
